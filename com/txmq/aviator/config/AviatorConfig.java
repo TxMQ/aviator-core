7638210ad1296c5cd6405def92a0b0d8953b427e
Craig Drabik <craig.drabik@txmq.com>
2018-10-26 15:16:10 -0400

    Changes to Reflection calls due to behavior change in Java 10.

--- com/txmq/aviator/config/AviatorConfig.java
+++ com/txmq/aviator/config/AviatorConfig.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import java.util.Map.Entry;
 
 import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -23,7 +25,7 @@ public class AviatorConfig {
 	
 	public static void loadConfiguration(String path) {
 		Map<String, Class<?>> processors = new HashMap<String, Class<?>>();
-		Reflections reflections = new Reflections();
+		Reflections reflections = new Reflections(new ConfigurationBuilder().addUrls(ClasspathHelper.forPackage("com.txmq")));
 		Set<Class<?>> processorClasses = reflections.getTypesAnnotatedWith(AviatorConfiguration.class);
 		for (Class<?> processorClass : processorClasses) {
 			AviatorConfiguration annotation = processorClass.getAnnotation(AviatorConfiguration.class);
